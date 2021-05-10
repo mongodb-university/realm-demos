477bbc956cb87dfa2bb68f7af01e9b1e18380806
am-MongoDB <andrew.morgan@mongodb.com>
2021-05-10 17:18:43 +0100

    add  scheme to allow Realm app id to be provided fromt the command line

--- inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoApp.swift
+++ inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoApp.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 import RealmSwift
 
-let app = RealmSwift.App(id: "inventorysync-xxxx") // TODO: Set the Realm application ID
+//let app = RealmSwift.App(id: "inventorysync-xxxx") // TODO: Set the Realm application ID
+let appId = ProcessInfo.processInfo.environment["REALM_APP_ID"]
+let app = RealmSwift.App(id: appId!)
 var store = "101"
 
 @main
