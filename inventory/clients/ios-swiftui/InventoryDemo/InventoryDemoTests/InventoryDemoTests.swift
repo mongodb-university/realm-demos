8a0d5d20299740f288a863f27df1ab91b771d2cd
Lauren Schaefer <lauren.schaefer@mongodb.com>
2021-05-13 10:04:20 -0400

    Intentionally fail a test

--- inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoTests/InventoryDemoTests.swift
+++ inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoTests/InventoryDemoTests.swift
@@ -21,7 +21,7 @@ class InventoryDemoTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(true)
+        XCTAssert(false)
     }
 
     func testPerformanceExample() throws {
