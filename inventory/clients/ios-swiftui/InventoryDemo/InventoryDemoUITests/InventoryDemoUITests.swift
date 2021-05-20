534035ab98c250ea5be6a45ec2b9a37b80b55d77
Diego Freniche <diego@freniche.com>
2021-05-20 13:18:03 +0200

    Added working UI test

--- inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoUITests/InventoryDemoUITests.swift
+++ inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoUITests/InventoryDemoUITests.swift
@@ -22,21 +22,16 @@ class InventoryDemoUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testClickLoginButton() throws {
         let app = XCUIApplication()
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+                           
+        let startButton = app.buttons["Log In"]
+        XCTAssertTrue(startButton.waitForExistence(timeout: 1))
+        XCTAssertTrue(startButton.isEnabled)
+        startButton.tap()
     }
 }
