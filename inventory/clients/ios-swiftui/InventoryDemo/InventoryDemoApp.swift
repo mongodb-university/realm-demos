013669e74f2930ce5574d79adaed7f84c3031bda
Diego Freniche <diego@freniche.com>
2021-05-25 18:37:01 +0200

    Added small comment

--- inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoApp.swift
+++ inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoApp.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 import RealmSwift
 
+// Important: you must create a text file in the root folder of this project (where the InventoryDemo.xcproject is)
+// called "realm-app-id.txt" and put inside a line with the realm app id you've created
+// that id will look like myappsync-xxxxx
 let (success, filecontent) = readFile(named: "realm-app-id.txt")
 
 let app = RealmSwift.App(id: success ? filecontent: "Couldn't read secrets file. Is there?" )
